25d53216fe936270de7317321ec469220e9e6af0
Son Dieu <sdieu@Sons-MacBook-Pro.local>
2020-06-19 11:21:17 +0700

    link to open dev

--- src/sdk-handler/index.jsx
+++ src/sdk-handler/index.jsx
@@ -1,6 +1,9 @@
+// @ts-nocheck
+/*global chrome*/
+
 import React, { useState, useEffect, useCallback, useContext } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons'
+import { faPlay, faStop, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import useQuery from "../hook/useQuery"
 import Context from "../context"
 
@@ -39,6 +42,14 @@ export default ({ accessToken }) => {
   return <>
     <h6 className="text-muted">SDK</h6>
     <button className="btn btn-primary mr-1" onClick={() => startSdk()}><FontAwesomeIcon icon={faPlay} /> Start </button>
-    <button className="btn btn-secondary" onClick={() => stopSdk()}><FontAwesomeIcon icon={faStop} /> End</button>
+    <button className="btn btn-secondary mr-1" onClick={() => stopSdk()}><FontAwesomeIcon icon={faStop} /> End</button>
+    <button className="btn btn-success mr-1" onClick={() => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tab = tabs[0]
+        const protocols = tab.url.split("//")[0];
+        const domain = tab.url.split("//")[1].split('/')[0]
+        chrome.tabs.create({ url: `${protocols}//${domain}/c-dev/` });
+      })
+    }}><FontAwesomeIcon icon={faExternalLinkAlt} /> Open Dev </button>
   </>
 }
